28dfa0919d0878c4fe55c96bcdb1bdefa0204bc0
dev-shani <zeeshan.ali60767@gmail.com>
2021-12-29 10:53:59 +0500

    Updated search tab

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,4 +24,49 @@ $(document).ready(() => {
 			}
 		}
 	});
+
+
+	$(document).on('change','input[name=flight_type]', function(){
+		console.log($(this).val());
+		let flightType = $(this).val();
+		if(flightType == 1){
+			$('.search-box').find('.tour_type').removeClass('d-block').addClass('d-none');
+			$('.search-box').find('.tour_type.single-tour').removeClass('d-none');
+			$('.search-box').find('.tour_type.single-tour .return input').removeAttr('disabled');
+			
+		}else if(flightType == 2){
+			$('.search-box').find('.tour_type').removeClass('d-block').addClass('d-none');
+			$('.search-box').find('.tour_type.single-tour').removeClass('d-none').addClass('d-block');
+			$('.search-box').find('.tour_type.single-tour .return input').attr('disabled','disabled');
+		}else if(flightType == 3){
+			$('.search-box').find('.tour_type').removeClass('d-block').addClass('d-none');
+			$('.search-box').find('.tour_type.multi-tour').removeClass('d-none').addClass('d-block');
+		}
+	})
+
+	$(document).on('click','.cls_dropdown', function(){
+		$(this).next('.cls_dd-menu').slideToggle();
+	})
+
+	$(document).on('click','.btn-add', function(){
+		let num = parseInt($(this).prev('input').val());
+		num++;
+		$(this).prev('input').val((num).toString());
+	})
+	
+	$(document).on('click','.btn-sub', function(){
+		let num = parseInt($(this).next('input').val());
+		if(num != 0){
+			num--;
+		}
+		$(this).next('input').val((num).toString());
+	})
+
+	$(document).on('click','.btn-add-tour', function(){
+		$(this).parents('.tour-ctrl').parents('.multi-tour').find('.st-form').first().clone().appendTo('.tour-append')
+	})
+	
+	$(document).on('click','.btn-remove-tour', function(){
+		$(this).parents('.tour-ctrl').prev('.tour-append').children().last().remove();
+	})
 })
